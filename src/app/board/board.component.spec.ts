4ba471d01bd103d87c965863faf8ed05542a6fc6
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2018-11-09 12:00:58 -0500

    Testing

--- src/app/board/board.component.spec.ts
+++ src/app/board/board.component.spec.ts
@@ -1,21 +1,61 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
+import { of } from 'rxjs';
 
 import { BoardComponent } from './board.component';
-import { By } from '@angular/platform-browser';
 import { State } from '../state.enum';
-import { of } from 'rxjs';
+import { TasksService } from '../tasks.service';
 
 describe('BoardComponent', () => {
   let component: BoardComponent;
   let fixture: ComponentFixture<BoardComponent>;
+  let routerMock: Router;
+  let tasksServiceMock: TasksService;
 
   beforeEach(async(() => {
+
+    routerMock = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
+    tasksServiceMock = jasmine.createSpyObj('TasksService', {
+      getTasksGrouped: of({
+        TODO: [
+          {
+            id: 1,
+            name: 'Task 1',
+            description: 'Description task 1',
+            date: new Date(2018, 0, 1),
+            state: State.TODO
+          },
+          {
+            id: 2,
+            name: 'Task 2',
+            description: 'Description task 2',
+            date: new Date(2018, 1, 1),
+            state: State.TODO
+          },
+          {
+            id: 3,
+            name: 'Task 3',
+            description: 'Description task 3',
+            date: new Date(2018, 2, 1),
+            state: State.TODO
+          }
+        ]
+      })
+    });
+
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [BoardComponent],
-      schemas: [NO_ERRORS_SCHEMA],
-      imports: [HttpClientTestingModule]
+      providers: [
+        { provide: Router, useValue: routerMock },
+        { provide: TasksService, useValue: tasksServiceMock }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
   }));
@@ -41,32 +81,6 @@ describe('BoardComponent', () => {
   });
 
   it('debe pintar 3 tareas en la columnas de TODO', () => {
-    component.tasks$ = of([
-      {
-        id: 1,
-        name: 'Task 1',
-        description: 'Description task 1',
-        date: new Date(2018, 0, 1),
-        state: State.TODO
-      },
-      {
-        id: 2,
-        name: 'Task 2',
-        description: 'Description task 2',
-        date: new Date(2018, 1, 1),
-        state: State.TODO
-      },
-      {
-        id: 3,
-        name: 'Task 3',
-        description: 'Description task 3',
-        date: new Date(2018, 2, 1),
-        state: State.TODO
-      }
-    ]);
-
-    fixture.detectChanges();
-
     const tasks = fixture.debugElement.queryAll(By.css('app-task'));
 
     expect(tasks.length).toBe(3);
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 import { State } from '../state.enum';
 import { TasksService } from '../tasks.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-board',
@@ -13,10 +14,17 @@ import { TasksService } from '../tasks.service';
 export class BoardComponent implements OnInit {
   tasks$: Observable<TasksGrouped>;
 
-  constructor(private tasksService: TasksService) { }
+  constructor(
+    private tasksService: TasksService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.tasks$ = this.tasksService.getTasksGrouped();
+  }
+
+  goToNewTask() {
+    this.router.navigateByUrl('/new-task');
   }
 
 }
